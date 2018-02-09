85255cee3b52fd95586de35b22a77db669d159c6
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-02-09 01:36:18 -0300

    Add files via upload

--- Sistema de Gestão/src/Recurso.java
+++ Sistema de Gestão/src/Recurso.java
@@ -1,85 +1,87 @@
-
 public class Recurso {
-	
-	private String ident;
-	private Date dInicio;
-	private Hour hInicio;
-	private Date dTermino;
-	private Hour htermino;
-	private String user;
-	private String status;
-	
-	public String getStatus() {
-		return status;
-	}
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    private String ident;
+    private Date dInicio;
+    private Hour hInicio;
+    private Date dTermino;
+    private Hour htermino;
+    private User user;
+    private String status = "Em processo de alocação";
+    private Atividade event;
 
-	public Recurso(String ident, Date dInicio, Hour hInicio, Date dTermino,
-			Hour htermino, String user) {
-		this.ident = ident;
-		this.dInicio = dInicio;
-		this.hInicio = hInicio;
-		this.dTermino = dTermino;
-		this.htermino = htermino;
-		
-		if(user != "Professor" || user != "Pesquisador" || user != "Administrador")
-			System.out.println("Usuario não permitido");
-		else 
-			this.user = user;
-		
-		status = "Em processo de alocação";
-	}
+    public Atividade getEvent(){
+        return this.event;
+    }
 
-	public String getIdent() {
-		return ident;
-	}
+    public void setEvent(Atividade event){
+        this.event = event;
+    }
 
-	public void setIdent(String ident) {
-		this.ident = ident;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public Date getdInicio() {
-		return dInicio;
-	}
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	public void setdInicio(Date dInicio) {
-		this.dInicio = dInicio;
-	}
+    public Recurso(String ident, Date dInicio, Hour hInicio, Date dTermino,
+                   Hour htermino, User user, Atividade event) {
+        this.ident = ident;
+        this.dInicio = dInicio;
+        this.hInicio = hInicio;
+        this.dTermino = dTermino;
+        this.htermino = htermino;
+        this.event = event;
+    }
 
-	public Hour gethInicio() {
-		return hInicio;
-	}
+    public String getIdent() {
+        return ident;
+    }
 
-	public void sethInicio(Hour hInicio) {
-		this.hInicio = hInicio;
-	}
+    public void setIdent(String ident) {
+        this.ident = ident;
+    }
 
-	public Date getdTermino() {
-		return dTermino;
-	}
+    public Date getdInicio() {
+        return dInicio;
+    }
 
-	public void setdTermino(Date dTermino) {
-		this.dTermino = dTermino;
-	}
+    public void setdInicio(Date dInicio) {
+        this.dInicio = dInicio;
+    }
 
-	public Hour getHtermino() {
-		return htermino;
-	}
+    public Hour gethInicio() {
+        return hInicio;
+    }
 
-	public void setHtermino(Hour htermino) {
-		this.htermino = htermino;
-	}
+    public void sethInicio(Hour hInicio) {
+        this.hInicio = hInicio;
+    }
 
-	public String getUser() {
-		return user;
-	}
+    public Date getdTermino() {
+        return dTermino;
+    }
 
-	public void setUser(String user) {
-		this.user = user;
-	}
-	
+    public void setdTermino(Date dTermino) {
+        this.dTermino = dTermino;
+    }
+
+    public Hour getHtermino() {
+        return htermino;
+    }
+
+    public void setHtermino(Hour htermino) {
+        this.htermino = htermino;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
 
 }
