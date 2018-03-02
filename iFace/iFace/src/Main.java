8899fd1923c4a9b4c628b2e2ba3703f76337a708
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-03-02 15:04:38 -0300

    Update

---
+++ iFace/iFace/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	/*
+	 * Criação de conta
+	 * Criar/modificar dados pessoais ()
+	 * Add amigos
+	 * Envio de mensagens
+	 * Criação de grupos de comunidade
+	 * Add membros aos grupos
+	 * Recuperar dados de um usuario, seja senha, email qq coisa
+	 * Remover conta 
+	 * */
+	
+
+}
